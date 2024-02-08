d16171d28a6fc58e1771f78d6ed9004e041aa7b0
Elli <ellistrong4@gmail.com>
2024-02-08 19:00:10 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-my-hometown",
+      "name": "My hometown",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8845949309930816,
+        "pitch": 0.26163016497420166,
+        "fov": 1.5264037754680837
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0949692296282354,
+          "pitch": 0.20985085535154013,
+          "rotation": 0,
+          "target": "1-my-hometown2"
+        },
+        {
+          "yaw": -0.012443111954153707,
+          "pitch": 0.13120291461165579,
+          "rotation": 0,
+          "target": "2-my-hometown3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.195949578408962,
+          "pitch": -0.10255951859410928,
+          "title": "My former home",
+          "text": "This is the place where I came from before living in Laguna at the age of 12.<div><br></div><div>A lot has been changed but still home is home.</div>"
+        },
+        {
+          "yaw": 2.5018267133920906,
+          "pitch": -0.00416723694976362,
+          "title": "Beloved Neigbors every new year",
+          "text": "Their fireworks is the one we've been looking forward every year.<div><br></div><div>They always gave us few fireworks to play with and the new year sky always look glamorous because of them.</div>"
+        },
+        {
+          "yaw": -0.9522759962306502,
+          "pitch": 0.09207412407321414,
+          "title": "Lolo manuel residents",
+          "text": "He was a very religious man, kind hearted but a little bit strict when it comes to our attitude."
+        }
+      ]
+    },
+    {
+      "id": "1-my-hometown2",
+      "name": "My hometown2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05868962336958816,
+          "pitch": 0.17148388872342757,
+          "rotation": 0,
+          "target": "0-my-hometown"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5082854387844744,
+          "pitch": -0.020952310307409405,
+          "title": "My Friend / Enemy teacher",
+          "text": "This is the placed where one of my love and hate teacher named Mrs. Manansala was currently living.<div><br></div><div><br></div>"
+        },
+        {
+          "yaw": 1.6399094589410375,
+          "pitch": 0.002641146855001608,
+          "title": "My friend House",
+          "text": "I remember when I was a child we are always playing at his house before I finally lived at Laguna.<div><br></div><div>Their house looks the same before I leave, I don't know if I should be happy or not?</div>"
+        },
+        {
+          "yaw": -0.6247005386832356,
+          "pitch": 0.20755774328677212,
+          "title": "Em-em House",
+          "text": "One of my childhood friend house who seems to be a talented student as of these days."
+        }
+      ]
+    },
+    {
+      "id": "2-my-hometown3",
+      "name": "My hometown3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1376404008793326,
+          "pitch": 0.11489334548415897,
+          "rotation": 0,
+          "target": "0-my-hometown"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.343404375889776,
+          "pitch": 0.0626286550107551,
+          "title": "My Favorite Bakeshop",
+          "text": "Every morning, my grandmother ensured that she already bought an Egg pie into this bakeshop for my breakfast.<div><br></div><div>Their Egg pie was very UNDERRATED!!</div>"
+        },
+        {
+          "yaw": -1.0107312867817004,
+          "pitch": 0.02873194446145355,
+          "title": "My Favorite vendor",
+          "text": "L:ola Gayang is one of the most kind hearted person that I've encounter.<div><br></div><div>She still remember me until now!!</div>"
+        },
+        {
+          "yaw": 1.3134110274809334,
+          "pitch": -0.10917289616221382,
+          "title": "Playground Memories",
+          "text": "Every noon me and one of my friend whose living here was playing at this yard until we get tired."
+        },
+        {
+          "yaw": -0.9870030784762545,
+          "pitch": 0.3158028494384215,
+          "title": "I think this was my cousin!!",
+          "text": "Her height, slippers, and the way she style was resembling my cousin haha!"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
